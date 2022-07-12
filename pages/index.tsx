4a18c08f0914ad4fd1c109f897e1a04f7441b320
嶋田印刷 <107801411+shimada-print@users.noreply.github.com>
2022-07-12 18:31:52 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Index({ allPosts }: Props) {
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Shimada_print Blog by Next.js with {CMS_NAME}</title>
         </Head>
         <Container>
           <Intro />
