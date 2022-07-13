56dff9e159285e69787391ec8bb910165b0efd06
嶋田印刷 <107801411+shimada-print@users.noreply.github.com>
2022-07-14 00:19:53 +0900

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,9 +4,9 @@ const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Blog.pre-render
       </h1>
-      markdownJSX：簡単に投稿・保管が出来て&Next.js；SEOで集客力が上がり&TypeScript：静的型付けなので正確
+      markdownJSXなので簡単に投稿・保管が出来て、Next.jsなのでSEOで集客力が上がり、TypeScriptは静的型付けなので正確
     </section>
   )
 }
