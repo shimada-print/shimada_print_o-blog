2851701f2cc7ced818c8c30643f9474f3108fbdf
嶋田印刷 <107801411+shimada-print@users.noreply.github.com>
2022-07-14 00:36:59 +0900

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,13 +9,16 @@ const Footer = () => {
           <div className="sp-ob_banner-1-1">
             <img src="/assets/blog/sp-ob_banner-1.jpg" alt="Shimada_Print Blog" className="sp-ob_banner-1-2" />
           </div>
-          <div className="GitHub_s-1-1 nowrap">
+          <div className="GitHub_s-1-1">
+            <table border="0">
+            <tr><td nowrap>
             <a
               href={`https://github.com/shimada-print/shimada-print-o-blog`}
               className="mx-3 font-bold hover:underline"
             >
               Source on GitHub
             </a>
+            </td></tr></table>
           </div>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
@@ -25,13 +28,16 @@ const Footer = () => {
               Shimada_Print Homepage
             </a>
           </div>
-          <div className="created_spc-1-1 nowrap">
+          <div className="created_spc-1-1">
+            <table border="0">
+            <tr><td nowrap>
             <a
               href={`https://docusaurus-2-shimada-print-1.vercel.app/docs/doc1`}
               className="mx-3 font-bold hover:underline"
             >
               created by S.P Consulting
             </a>
+            </td></tr></table>
           </div>
         </div>
       </Container>
