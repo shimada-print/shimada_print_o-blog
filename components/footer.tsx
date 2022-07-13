0af1f456fc03d86e40b9910d0f2e590a16f23a23
嶋田印刷 <107801411+shimada-print@users.noreply.github.com>
2022-07-13 22:29:09 +0900

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,13 +6,9 @@ const Footer = () => {
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-             A statically generated blog example using
-            <a href="https://nextjs.org/" lassName="underline hover:text-blue-600 duration-200 transition-colors">
-              Next.js
-            </a>
-            and {CMS_NAME}.
-          </h4>
+          <div className="sp-ob_banner-1-1">
+            <img src="/assets/blog/sp-ob_banner-1.jpg" alt="" className="sp-ob_banner-1-2">
+          </div>            
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="http://shimada.starfree.jp/"
