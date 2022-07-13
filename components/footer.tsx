c8df642d4bcec2d2d6aa27f71ca768cd8cba395a
嶋田印刷 <107801411+shimada-print@users.noreply.github.com>
2022-07-13 23:39:57 +0900

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -17,7 +17,7 @@ const Footer = () => {
               <nobr>Source on GitHub</nobr>
             </a>
           </div>
-          <div ア className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="http://shimada.starfree.jp/"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
