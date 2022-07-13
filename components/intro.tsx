f54389401bcbb112548a05b27e9c3198eac56bb2
嶋田印刷 <107801411+shimada-print@users.noreply.github.com>
2022-07-14 00:17:31 +0900

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,9 +6,7 @@ const Intro = () => {
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
       </h1>
-      markdownJSX：簡単に投稿・保管が出来て&Next.js；SEOで集客力が上がり&TypeScript：静的型付けなので正確</li>
-        </ul>
-      </h4>
+      markdownJSX：簡単に投稿・保管が出来て&Next.js；SEOで集客力が上がり&TypeScript：静的型付けなので正確
     </section>
   )
 }
