852002fa35f90640732e842853c3263fbc8cc62e
嶋田印刷 <107801411+shimada-print@users.noreply.github.com>
2022-07-14 16:24:21 +0900

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -4,12 +4,12 @@ const Header = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/">
-        <img src="/assets/blog/sp-ob_banner-1.jpg" alt="Shimada_Print Blog" 
-         className="sp-ob_banner-1-2 width:300px height:85px" width="300" height="85" />
         <a className="hover:underline">Blog</a>
       </Link>
       .
     </h2>
+    <img src="/assets/blog/sp-ob_banner-1.jpg" alt="Shimada_Print Blog" 
+    className="sp-ob_banner-1-2 width:300px height:85px" width="300" height="85" />
   )
 }
 
