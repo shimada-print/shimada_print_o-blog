46f01802b31745b458b6de19d60cab5aa422ca64
嶋田印刷 <107801411+shimada-print@users.noreply.github.com>
2022-07-14 16:16:01 +0900

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -8,6 +8,10 @@ const Intro = () => {
       </h1>
       markdownJSXなので簡単に投稿・保管が出来て、Next.jsなのでSEOで集客力が上がり、TypeScriptは静的型付けなので正確
     </section>
+    <div className="sp-ob_banner-1-1">
+      <img src="/assets/blog/sp-ob_banner-1.jpg" alt="Shimada_Print Blog" 
+      className="sp-ob_banner-1-2 width:300px height:85px" width="300" height="85" />
+    </div>
   )
 }
 
